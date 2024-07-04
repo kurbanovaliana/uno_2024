87e94e1a2fa8024e935362a1302413d7c3469bd7
liana <lianakurbanowa@gmail.com>
2024-07-04 07:47:03 +0300

    + текстовый интерфейс

--- правила, json.py
+++ правила, json.py
@@ -11,6 +11,57 @@
 6) у оставшихся игроков подсчитываются очки( по значению карты)
 '''
 
+# Текстовый интерфейс
+'''
+Top: g2
+Sam: r3 y5 g4 r1
+Sam play: g4
+-----
+Top: g4
+Pit: y6 y2 r8 g7 g5
+Pit play: g7
+----
+Top: g7
+Sam: r3 y5 r1
+Sam : takes the card
+Sam : r3 y5 r1 r4
+Sam : pass
+----
+Top: g7
+Pit: y6 y2 r8 g5
+Pit play: g5
+---
+Top: g5
+Sam: r3 y5 r1 r4
+Sam play: y5
+----
+Top: y5
+Pit: y6 y2 r8
+Pit play : y2
+----
+Top : y2
+Sam : r3 r1 r4
+Sam : takes the card
+Sam : r3 r1 r4 r5
+Sam : pass
+----
+Top : y2
+Pit : y6 r8
+Pit play : y6 
+----
+Top : y6
+Sam : r3 r1 r4 r5
+Sam: takes the card
+Sam : r3 r1 r4 r5 y8
+Sam play : y8 
+----
+Top : y8
+Pit: r8
+Pit play : r8
+----
+Pit win!!!
+'''
+
 
 ## Пример save-файла
 '''
